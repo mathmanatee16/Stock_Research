07a49dcf37cf88e745f0267fb149def62cb1fc1e
mathmanatee16 <141573844+mathmanatee16@users.noreply.github.com>
2023-09-29 14:56:00 -0400

    Create Tesla.py

---
+++ Tesla.py
@@ -0,0 +1,7 @@
+!pip install yfinance==0.2.4
+import yfinance as yf
+import pandas as pd
+tesla=yf.Ticker("TSLA")
+tesla_data=tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
